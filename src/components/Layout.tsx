08fa569c038adeb1f376890c0b8b2585e38df47f
David Battye <battye_david@yahoo.co.uk>
2025-09-30 14:02:39 +0100

    added s to the config file https

--- src/components/Layout.tsx
+++ src/components/Layout.tsx
@@ -1,4 +1,4 @@
-import React, { type PropsWithChildren } from 'react';
+import { type PropsWithChildren } from 'react';
 import Header from './Header';
 
 const Layout = ({ children }: PropsWithChildren) => {
